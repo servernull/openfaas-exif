c2126cca86e321f2b7b00d563f1b9c3d38d091d0
Scott Lee Davis <skawtus@gmail.com>
2019-10-24 21:54:14 -0700

    remove unused json annotations.

--- openfaas-exif/handler.go
+++ openfaas-exif/handler.go
@@ -13,16 +13,16 @@ import (
 )
 
 type IfdEntry struct {
-	IfdPath     string      `json:"ifd_path"`
-	FqIfdPath   string      `json:"fq_ifd_path"`
-	IfdIndex    int         `json:"ifd_index"`
-	TagId       uint16      `json:"tag_id"`
-	TagName     string      `json:"tag_name"`
-	TagTypeId   uint16      `json:"tag_type_id"`
-	TagTypeName string      `json:"tag_type_name"`
-	UnitCount   uint32      `json:"unit_count"`
-	Value       interface{} `json:"value"`
-	ValueString string      `json:"value_string"`
+	IfdPath     string
+	FqIfdPath   string
+	IfdIndex    int
+	TagId       uint16
+	TagName     string
+	TagTypeId   uint16
+	TagTypeName string
+	UnitCount   uint32
+	Value       interface{}
+	ValueString string
 }
 
 // Handle a serverless request
